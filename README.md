9edb74723a6bd07f3a0275ab89e0ed82ccb4036e
Zhang <zhang@wavelet.net>
2023-04-12 11:29:15 +0800

    name check

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# master
+# User guide
+1. create virtual environment:
+python3 -m venv venv
+
+2. activate venv:
+source venv/bin/activate
+
+3. install all the required package:
+pip3 install -r requirements.txt
+
+4. run application: 
+python3 application.py
---
+++ bilibili-fyp-backend/src/db/mongo_db.py
@@ -0,0 +1,26 @@
+import os
+
+from pymongo import MongoClient
+import pprint
+import dotenv
+
+# this should be put in application.py
+dotenv.load_dotenv()
+
+MONGO_DB_CONNECTION = os.getenv('MONGO_DB_CLUSTER_ENDPOINT')
+
+
+# connect to cluster
+client = MongoClient(MONGO_DB_CONNECTION)
+
+# select database, select collection
+db = client['FIT4701']
+collection = db['Data']
+
+# print certain documents
+all_documents = collection.find({"CODE": {"$lt": 5}})
+for document in all_documents:
+    pprint.pprint(document)
+
+# print document count
+print(collection.count_documents({}))
