78b996f5f69102f2ca2a54781a998dd6e23512ca
ylim0066 <robert.merkel@benambra.org>
2023-04-19 18:13:00 +0800

    updated the test file to call the directory from os rather than hard coding

--- src/WEKA/test.py
+++ src/WEKA/test.py
@@ -1,3 +1,5 @@
+import os
+
 import weka.core.jvm as jvm
 from weka.core.classes import Random
 from weka.core.converters import Loader
@@ -5,9 +7,11 @@ from weka.classifiers import PredictionOutput, KernelClassifier, Kernel, Evaluat
 
 jvm.start(system_cp=True, packages=True, max_heap_size="512m")
 
-ml_data_dir = "C:/Users/Asus/Documents/FIT4701/FYP_repo/bilibili-fyp-backend/src/WEKA/datasets/"
+print(os.getcwd())
+
+ml_data_dir = "/datasets/"
 loader = Loader(classname="weka.core.converters.ArffLoader")
-ml_data = loader.load_file(ml_data_dir + "MLDATA.csv.arff")
+ml_data = loader.load_file(os.getcwd() + ml_data_dir + "MLDATA.csv.arff")
 ml_data.class_is_last()
 print(ml_data)
 
