ffb9ccf6963132ac9a8b25dd90e82078d0fb82ee
TaoAstral <tzha0079@student.monash.edu>
2023-06-07 23:53:07 +0800

    update delete_dataset function

--- src/ml/preprocessing.py
+++ src/ml/preprocessing.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 from src.db import mongo_db_function
 
+db = mongo_db_function.get_database('FIT4701')
+collection = mongo_db_function.get_collection(db, "Data")
+mongo_db_function.delete_dataset(collection, "000")
 
 def missing_values(dataset_id):
     # Preprocessing with string "n/a"
@@ -23,28 +26,31 @@ def missing_values(dataset_id):
 
     db = mongo_db_function.get_database('FIT4701')
     collection = mongo_db_function.get_collection(db, "Data")
-    store = mongo_db_function.get_by_query(collection, dataset_id, "DATASET_ID")
 
-    # print(store[0].values())
-    db_data = []
-    keys = []
-    for item in store:
-        item.pop('DATASET_ID')
-        item.pop('_id')
-        values = list(item.values())
-        db_data.append(values)
+    # store = mongo_db_function.get_by_query(collection, dataset_id, "DATASET_ID")
 
-    # print(db_data)
 
-    df = pd.DataFrame(data=db_data)
-    df = df.replace("n/a", np.nan)
 
-    arr = df.values
-    # print(arr)
-    imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
-    arr_new = imp_mean.fit_transform(arr)
-    # print(type(arr_new[0][0]))
-    # print(arr_new)
+    # # print(store[0].values())
+    # db_data = []
+    # keys = []
+    # for item in store:
+    #     item.pop('DATASET_ID')
+    #     item.pop('_id')
+    #     values = list(item.values())
+    #     db_data.append(values)
+    #
+    # # print(db_data)
+    #
+    # df = pd.DataFrame(data=db_data)
+    # df = df.replace("n/a", np.nan)
+    #
+    # arr = df.values
+    # # print(arr)
+    # imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
+    # arr_new = imp_mean.fit_transform(arr)
+    # # print(type(arr_new[0][0]))
+    # # print(arr_new)
 
 
 # missing_values(dataset_id={"DATASET_ID": "6480818a2b02836f0686e027"})
